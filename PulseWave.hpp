547845c55a5a34a08bd087d1848112b802bdae81
ShaigHamzaliyev <h.shaig93@gmail.com>
2023-07-24 14:24:58 +0200

    pulse wave

---
+++ PulseWave.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#ifndef PULSEWAVES_HPP
+#define PULSEWAVES_HPP
+
+#include <string>
+#include <vector>
+
+class VLR {
+    // Declare VLR class members and methods here
+};
+
+class PulseRecord {
+    // Declare PulseRecord class members and methods here
+};
+
+class Waves {
+    // Declare Waves class members and methods here
+};
+
+class PulseWaves {
+public:
+    PulseWaves(const std::string& pls_file);
+
+    // Define PulseWaves class methods here
+
+private:
+    // Define PulseWaves class members here
+};
+
+#endif // PULSEWAVES_HPP
